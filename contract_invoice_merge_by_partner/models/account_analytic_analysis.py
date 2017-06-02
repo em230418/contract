6f93b698282082f40421bed8e16efe0dc124840a
Carlos Dauden <carlos.dauden@tecnativa.com>
2017-06-02 12:44:09 +0200

    [9.0][FIX] contract_invoice_merge_by_partner: Merge only new invocices

--- contract_invoice_merge_by_partner/models/account_analytic_analysis.py
+++ contract_invoice_merge_by_partner/models/account_analytic_analysis.py
@@ -12,23 +12,15 @@ class AccountAnalyticAccount(models.Model):
 
     @api.multi
     def recurring_create_invoice(self):
-        contracts = self.search(
-            [('recurring_next_date', '<=', fields.Date.today()),
-             ('account_type', '=', 'normal'),
-             ('recurring_invoices', '=', True)]
-        )
-        res = super(AccountAnalyticAccount, self).recurring_create_invoice()
-        if not contracts:
-            return res
-        invoices = self.env['account.invoice'].search([
-            ('contract_id', 'in', contracts.ids)
-        ])
+        invoices = super(
+            AccountAnalyticAccount, self).recurring_create_invoice()
+        invoices_info = {}
         invoices2unlink = self.env['account.invoice']
         for partner in invoices.mapped('partner_id'):
             invoices2merge = invoices.filtered(
                 lambda x: x.partner_id == partner)
             if partner.contract_invoice_merge and len(invoices2merge) > 1:
-                invoices2merge.do_merge()
+                invoices_info.update(invoices2merge.do_merge())
                 invoices2unlink += invoices2merge
         invoices2unlink.unlink()
-        return True
+        return invoices.browse(invoices_info.keys())
